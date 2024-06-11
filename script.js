a5a3d5e95e079c643a1046c7357cb6506c2a14c7
Øystein <oystein.koder@gmail.com>
2024-06-11 09:57:57 +0200

    Lagt inn endring av tekst

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-
+const change_h1 = document.getElementById("changed_text");
+change_h1.textContent = "Denne teksten er endret med javaScript";
 
 const new_h1 = document.createElement("h1");
 new_h1.textContent = "Denne teksten og paragrafen er generert med javaScript";
