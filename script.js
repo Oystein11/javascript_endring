659f571d2ab7e1e6de087e256e4edc137f152b9a
Øystein <oystein.koder@gmail.com>
2024-06-11 09:48:56 +0200

    Lagt inn java-generert tekst

---
+++ script.js
@@ -0,0 +1,6 @@
+
+
+const new_h1 = document.createElement("h1");
+new_h1.textContent = "Denne teksten og paragrafen er generert med javaScript";
+new_h1.id = "my_h1";
+document.getElementById("box2").append(new_h1);
